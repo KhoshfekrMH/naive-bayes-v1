049165d0dc680a40bac0e55a476362b32bb0b457
brainy <khoshfekr2001@protonmail.com>
2022-11-13 04:11:00 +0330

    Average of both IQ and pass/fail column on averageIQ and averagePassAndFail

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas as pd
-from openpyxl import Workbook
 
 dataSets = pd.read_excel('dataset.xlsx')
 
 IQ = dataSets[1]
 passAndFail = dataSets[2]
+
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+
+averageIQ = average(IQ)
+averagePassAndFail = average(passAndFail)
+
